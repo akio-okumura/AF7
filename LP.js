6d78d5c7c2b9b9ef5bb688cdf73d427cce786a68
akio <nrtrstar@yahoo.co.jp>
2018-08-24 02:32:31 +0900

    herfがnullだとindex.htmlに戻り続ける

--- LP.js
+++ LP.js
@@ -2,7 +2,3 @@ var camera = document.querySelector('#camera');
 var camera_pos = camera.getAttribute('position');
 
 // console.log(camera_pos.z);
-
-if(camera_pos.z >= 27) {
-  document.querySelector('#portal').navigate('space.html');
-}
