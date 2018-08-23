4a6d2af3259fce502bc0af03322f2fbfea067c35
akio <nrtrstar@yahoo.co.jp>
2018-08-24 02:44:47 +0900

    jsからタイミング指定出来るかどうかのコミット

--- LP.js
+++ LP.js
@@ -2,3 +2,4 @@ var camera = document.querySelector('#camera');
 var camera_pos = camera.getAttribute('position');
 
 // console.log(camera_pos.z);
+document.querySelector('#portal').emit('url_jump');
