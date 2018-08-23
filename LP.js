365912ccb74c3b4f8817342252c459c4ebf5d7b8
akio <nrtrstar@yahoo.co.jp>
2018-08-24 02:29:47 +0900

    jsからherf指定

--- LP.js
+++ LP.js
@@ -1,4 +1,8 @@
 var camera = document.querySelector('#camera');
 var camera_pos = camera.getAttribute('position');
 
-console.log(camera_pos.z);
+// console.log(camera_pos.z);
+
+if(camera_pos.z >= 27) {
+  document.querySelector('#portal').navigate('space.html');
+}
