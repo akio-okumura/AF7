13c1ae80c777fbf344ae9d2d1b571d72e7b76237
akio <nrtrstar@yahoo.co.jp>
2018-08-24 14:48:21 +0900

    インターバル定義

--- LP.js
+++ LP.js
@@ -7,16 +7,6 @@ var camera_pos = camera.getAttribute('position');
 window.onload = function(){
   var check_campos = setInterval( function() {
     if(camera_pos.z >= 15){
-      var sceneEl = document.querySelector('a-scene');
-      var entityEl = document.createElement('a-entity');
-      entityEl.setAttribute('geometry', {
-        primitive: 'box',
-        height: 3,
-        width: 1
-      });
-      entityEl.setAttribute('position', {x: 0, y: 2, z: -3});
-      // Do `.setAttribute()`s to initialize the entity.
-      sceneEl.appendChild(entityEl);
       console.log("z >= 15 now");
     }
   }, 100);
