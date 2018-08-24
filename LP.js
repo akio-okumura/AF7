09ebe2a7ca7d0612a21d4336b482a367e1bd09eb
akio <nrtrstar@yahoo.co.jp>
2018-08-24 16:08:05 +0900

    welcome

--- LP.js
+++ LP.js
@@ -4,12 +4,17 @@ var camera_pos = camera.getAttribute('position');
 // console.log(camera_pos.z);
 // document.querySelector('#portal').emit('url_jump');
 
+var brick_deleted = false;
+
 window.onload = function(){
   var check_campos = setInterval( function() {
-    if(camera_pos.z <= 1){
-      console.log("z <= 1 now");
-      var brick_scene = document.querySelector('#BrickScene');
-      brick_scene.setAttribute('visible', 'false');
+    if(!brick_deleted){
+      if(camera_pos.z <= 1){
+        console.log("z <= 1 now");
+        var brick_scene = document.querySelector('#BrickScene');
+        brick_scene.setAttribute('visible', 'false');
+        brick_deleted = true;
+      }
     }
   }, 100);
 };
