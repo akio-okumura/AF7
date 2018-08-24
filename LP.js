58b92de0df863e3672d1e9aa9eb7dce732679676
akio <nrtrstar@yahoo.co.jp>
2018-08-24 19:05:55 +0900

    out分の処理

--- LP.js
+++ LP.js
@@ -20,3 +20,9 @@ window.onload = function(){
     }
   }, 100);
 };
+
+var fade_out_welcome = setInterval( function() {
+  var w_txt = document.querySelector('#welcome');
+  if(w_txt.opacity == 1.0)
+    w_txt.emit('out');
+});
