ddf18d3f564f8318e50bd3e9112c8941f36362f1
akio <nrtrstar@yahoo.co.jp>
2018-08-24 14:41:24 +0900

    js監視式

--- LP.js
+++ LP.js
@@ -4,16 +4,20 @@ var camera_pos = camera.getAttribute('position');
 // console.log(camera_pos.z);
 // document.querySelector('#portal').emit('url_jump');
 
-if(camera_pos.z >= 15){
-  var sceneEl = document.querySelector('a-scene');
-  var entityEl = document.createElement('a-entity');
-  entityEl.setAttribute('geometry', {
-    primitive: 'box',
-    height: 3,
-    width: 1
-  });
-  entityEl.setAttribute('position', {x: 0, y: 2, z: -3});
-  // Do `.setAttribute()`s to initialize the entity.
-  sceneEl.appendChild(entityEl);
-  console.log("z >= 15 now")
+window.onload{
+  var check_campos( function() {
+    if(camera_pos.z >= 15){
+      var sceneEl = document.querySelector('a-scene');
+      var entityEl = document.createElement('a-entity');
+      entityEl.setAttribute('geometry', {
+        primitive: 'box',
+        height: 3,
+        width: 1
+      });
+      entityEl.setAttribute('position', {x: 0, y: 2, z: -3});
+      // Do `.setAttribute()`s to initialize the entity.
+      sceneEl.appendChild(entityEl);
+      console.log("z >= 15 now")
+    }
+  }, 100);
 }
