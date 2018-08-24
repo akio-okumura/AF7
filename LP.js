ae000a896faec126185868de7821f1b0b64fea81
akio <nrtrstar@yahoo.co.jp>
2018-08-24 14:28:16 +0900

    box add

--- LP.js
+++ LP.js
@@ -3,3 +3,16 @@ var camera_pos = camera.getAttribute('position');
 
 // console.log(camera_pos.z);
 // document.querySelector('#portal').emit('url_jump');
+
+if(camera_pos.z >= 15){
+  var sceneEl = document.querySelector('a-scene');
+  var entityEl = document.createElement('a-entity');
+  entityEl.setAttribute('geometry', {
+    primitive: 'box',
+    height: 3,
+    width: 1
+  }
+  'position', {x: 0, y: 2, z: -3});
+  // Do `.setAttribute()`s to initialize the entity.
+  sceneEl.appendChild(entityEl);
+}
